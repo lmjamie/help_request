aec95858fe1b33b05d3ee45ca8caeb915ad6afaf
Landon Jamieson <landonjamieson@gmail.com>
2017-07-17 15:23:15 -0600

    Fix clock bug fix on heroku

--- public/js/onReady.js
+++ public/js/onReady.js
@@ -32,41 +32,39 @@ function setupAllClocks() {
 }
 
 function setupServingClocks() {
-  if (typeof clocks.serving !== 'undefined')
-    clocks.serving.forEach(function (s) {
-      s.stop();
+  if (typeof clocks.serInc !== 'undefined')
+    clocks.serInc.forEach(function (s) {
+      clearInterval(s);
     });
   clocks.serving = [];
+  clocks.serInc = [];
   $("#serving .clock").each(function (i) {
     var clockDiv = $(this);
     clocks.serving[i] = clockDiv.FlipClock(parseInt(
       (Date.now() - clockDiv.attr("data-started")) / 60000), {
       clockFace: "Counter"
     });
-    setTimeout(function () {
-      setInterval(function () {
-        clocks.serving[i].increment();
-      }, 60000);
-    });
+    setInterval(function () {
+      clocks.serInc[i] = clocks.serving[i].increment();
+    }, 60000);
   });
 }
 
 function setupCurrentClocks() {
-  if (typeof clocks.current !== 'undefined')
-    clocks.current.forEach(function (c) {
-      c.stop();
+  if (typeof clocks.curInc !== 'undefined')
+    clocks.curInc.forEach(function (c) {
+      clearInterval(c);
     });
   clocks.current = [];
+  clocks.curInc = [];
   $("#current .clock").each(function (i) {
     var clockDiv = $(this);
     clocks.current[i] = clockDiv.FlipClock(parseInt(
       (Date.now() - clockDiv.attr("data-started")) / 60000), {
       clockFace: "Counter"
     });
-    setTimeout(function () {
-      setInterval(function () {
-        clocks.current[i].increment();
-      }, 60000);
-    });
+    setInterval(function () {
+      clocks.curInc[i] = clocks.current[i].increment();
+    }, 60000);
   });
 }
